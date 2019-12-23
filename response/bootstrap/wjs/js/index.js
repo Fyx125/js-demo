410fc61eb87a88d28c74c0706f3ae32f0a04e2fb
yx666 <1251973330@qq.com>
2019-12-23 18:15:30 +0800

    bs

---
+++ response/bootstrap/wjs/js/index.js
@@ -0,0 +1,47 @@
+$(function () {
+
+    //初始化工具提示
+    $('[data-toggle="tooltip"]').tooltip();
+
+    //获取所有item
+    var items = $('.carousel-inner .item');
+    //监听屏幕尺寸大小改变
+    $(window).on('resize', function () {
+        var width = $(window).width();
+        if (width >= 768) {
+            <!--非移动端-->
+            $(items).each(function (index, value) {
+                var item = $(this);
+                var imgSrc = item.data('largeImage');
+                item.html($('<a href="javascript:void(0);" class="pcImg"></a>').css("background-image", "url('" + imgSrc + "')"));
+            })
+        } else {
+            <!--移动端-->
+            $(items).each(function (index, value) {
+                var item = $(this);
+                var imgSrc = item.data('smallImage');
+                item.html('<a href="javascript:void(0);" class="mobileImg"><img src="' + imgSrc + '" alt="..."></a>');
+            })
+        }
+    }).trigger('resize');
+
+    //添加移动端的滑动操作
+    var startX,endX;
+    var carousel_inner = $('.carousel-inner')[0];
+
+    var carousel = $('.carousel');
+    carousel_inner.addEventListener('touchstart',function (e) {
+        startX = e.targetTouches[0].clientX;
+    });
+    carousel_inner.addEventListener('touchend',function (e) {
+        endX = e.changedTouches[0].clientX;
+        if (endX - startX > 0) {
+            carousel.carousel('prev');
+        } else if (endX - startX < 0) {
+            carousel.carousel('next');
+        }
+    })
+
+    //添加导航栏的移动端滑动效果
+    
+});
